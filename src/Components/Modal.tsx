9df4da16e3e49bb8741351b4821b409e8fe7c2c1
Matthew <matitzo@o2.pl>
2023-02-28 22:48:18 +0100

    Modal styled component css.

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { StyledModal } from "../Styles/Modal.styled";
-// @ts-ignore
-import styles from "../Styles/Modal.module.css";
+import {
+  StyledModal,
+  StyledModalTextPosition,
+  StyledModalDiv,
+  StyledModalBody,
+  StyledModalWindow,
+} from "../Styles/Modal.styled";
 
 export default function GetModalHtml({ product, isModal, handleModal }) {
   return (
@@ -10,30 +14,34 @@ export default function GetModalHtml({ product, isModal, handleModal }) {
         display: `${isModal ? "flex" : "none"}`,
       }}
     >
-      <div
-        style={{
-          backgroundColor: `${product.color}`,
-        }}
-      >
-        <h3>Product informations</h3>
-        <span onClick={() => handleModal()}>&times;</span>
-        <div>
-          <div>
-            <p>Id:</p>
-            <p>Name:</p>
-            <p>Year:</p>
-            <p>Color:</p>
-            <p>Pantone Value:</p>
-          </div>
-          <div>
-            <p>{product.id}</p>
-            <p>{product.name}</p>
-            <p>{product.year}</p>
-            <p>{product.color}</p>
-            <p>{product.pantone_value}</p>
-          </div>
-        </div>
-      </div>
+      <StyledModalWindow>
+        <StyledModalDiv
+          style={{
+            backgroundColor: `${product.color}`,
+          }}
+        >
+          <header>
+            <h3>Product informations</h3>
+            <span onClick={() => handleModal()}>&times;</span>
+          </header>
+          <StyledModalBody>
+            <StyledModalTextPosition side="right">
+              <p>Id:</p>
+              <p>Name:</p>
+              <p>Year:</p>
+              <p>Color:</p>
+              <p>Pantone Value:</p>
+            </StyledModalTextPosition>
+            <StyledModalTextPosition side="left">
+              <p>{product.id}</p>
+              <p>{product.name}</p>
+              <p>{product.year}</p>
+              <p>{product.color}</p>
+              <p>{product.pantone_value}</p>
+            </StyledModalTextPosition>
+          </StyledModalBody>
+        </StyledModalDiv>
+      </StyledModalWindow>
     </StyledModal>
   );
 }
--- src/Styles/Modal.styled.js
+++ src/Styles/Modal.styled.js
@@ -14,3 +14,46 @@ export const StyledModal = styled.div`
   flex-direction: column;
   justify-content: center;
 `;
+
+export const StyledModalWindow = styled.div`
+  margin: 0 auto;
+  width: 350px;
+
+  header {
+    display: flex;
+    justify-content: center;
+  }
+
+  span {
+    color: black;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    position: relative;
+    top: -8px;
+    left: 15px;
+  }
+
+  span:hover,
+  span:focus {
+    color: #aaa;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+export const StyledModalTextPosition = styled.div`
+  text-align: ${({ side }) => (side === "right" ? "right" : "left")};
+  font-weight: ${({ side }) => side === "right" && "bold"};
+  width: 50%;
+  margin: 0 5px;
+`;
+
+export const StyledModalDiv = styled.div`
+  padding: 1.2em 0;
+  border-radius: 25px;
+`;
+
+export const StyledModalBody = styled.main`
+  display: flex;
+`;
